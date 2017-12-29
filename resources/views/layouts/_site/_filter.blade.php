87432a716308b9426e31df9d90cc80b23392e850
Benjamim Thiago <bebenjamimthiago@gmail.com>
2017-12-29 20:52:59 -0300

    finalizado o layout do filtro

--- resources/views/layouts/_site/_filter.blade.php
+++ resources/views/layouts/_site/_filter.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<form>
-		<div class="input-field col s6 m4">
+		<div class="input-field col s12 m4">
 			<select>
 				<option value="aluga">Aluga</option>
 				<option value="vende">Vende</option>
@@ -8,7 +8,7 @@
 			<label>Status</label>
 		</div>
 
-		<div class="input-field col s6 m4">
+		<div class="input-field col s12 m4">
 			<select>
 				<option value="1">Alvenaria</option>
 				<option value="2">Apartamento</option>
@@ -18,5 +18,68 @@
 			</select>
 			<label>Tipo de Imóvel</label>
 		</div>
+
+		<div class="input-field col s12 m4">
+			<select>
+				<option value="1">Maceió</option>
+				<option value="2">Arapiraca</option>
+				<option value="3">Teresina</option>
+				<option value="4">Picos</option>
+				<option value="5">Piripiri</option>
+				<option value="6">Floriano</option>
+			</select>
+			<label>Cidade</label>
+		</div>
+
+		<div class="input-field col s12 m1">
+			<select>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">Mais de Três</option>
+			</select>
+			<label>Dormitórios</label>
+		</div>
+
+		<div class="input-field col s12 m4">
+			<select>
+				<option value="1">Até R$500,00</option>
+				<option value="2">R$500,00 a R$1.000,00</option>
+				<option value="3">R$1.000,00 a R$5.000,00</option>
+				<option value="4">R$5.000,00 a R$10.000,00</option>
+				<option value="5">R$10.000,00 a R$15.000,00</option>
+				<option value="6">R$15.000,00 a R$20.000,00</option>
+				<option value="7">R$20.000,00 a R$25.000,00</option>
+				<option value="8">R$25.000,00 a R$30.000,00</option>
+				<option value="9">R$30.000,00 a R$35.000,00</option>
+				<option value="10">R$35.000,00 a R$40.000,00</option>
+				<option value="11">R$40.000,00 a R$45.000,00</option>
+				<option value="12">R$45.000,00 a R$50.000,00</option>
+				<option value="13">R$50.000,00 a R$55.000,00</option>
+				<option value="14">R$55.000,00 a R$60.000,00</option>
+				<option value="15">R$60.000,00 a R$65.000,00</option>
+				<option value="16">R$65.000,00 a R$70.000,00</option>
+				<option value="17">R$70.000,00 a R$75.000,00</option>
+				<option value="18">R$75.000,00 a R$80.000,00</option>
+				<option value="19">R$80.000,00 a R$85.000,00</option>
+				<option value="20">R$85.000,00 a R$90.000,00</option>
+				<option value="21">R$90.000,00 a R$95.000,00</option>
+				<option value="22">R$95.000,00 a R$100.000,00</option>
+				<option value="23">R$100.000,00 a 500.000,00</option>
+				<option value="24">R$500.000,00 a R$1.000.000,00</option>
+				<option value="25">Mais de R$1.000.000,00</option>
+			</select>
+			<label>Valor</label>
+		</div>
+
+		<div class="input-field col s12 m4">
+			<input class="validate" type="text" name="neighborhood" placeholder="Digite o bairro">
+			<label>Bairro</label>
+		</div>
+
+		<div class="input-field col s12 m3">
+			<button class="btn deep-orange darken-1 right col s12">Filtrar</button>
+		</div>
+
 	</form>
 </div>
